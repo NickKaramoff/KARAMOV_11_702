e31bd1e889a07fc900b02c2a40c74a682db7a52d
Nick Karamoff <nikita.karamoff@gmail.com>
2017-09-28 22:08:00 +0300

    Add ExpandableArray class based on previous work

---
+++ List/src/ru/karamoff/ExpandableArray.java
@@ -0,0 +1,70 @@
+package ru.karamoff;
+
+/** Расширяемый массив целых чисел.
+ * Поддерживает добавление элемента в конец, удаление элемента, вставку элемента в определённую позицию и вывод в консоль.
+ * @author Nick Karamoff
+ * @version 1.0
+ */
+
+class ExpandableArray {
+    private int array[] = new int[10];
+    int count = 0;
+
+    void appendItem(int element) {
+        if (count < array.length) {
+            array[count] = element;
+            count++;
+        } else {
+            expand();
+            appendItem(element);
+        }
+    }
+
+    void removeItem(int index) {
+        if (count > 0) {
+            for (int i = index; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            count -= 1;
+        } else {
+            System.err.println("Массив пуст!");
+        }
+    }
+
+    void insertItem(int index, int element) {
+        if (count < array.length) {
+            for (int i = count - 1; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+            System.out.println("Элемент добавлен в позицию " + index + ".");
+        } else {
+            expand();
+            insertItem(index, element);
+        }
+    }
+
+    void printArray() {
+        if (count > 0) {
+            System.out.println("Введённый массив:");
+            for (int i = 0; i < count; i++) {
+                System.out.print(array[i] + " ");
+            }
+            System.out.println();
+        } else {
+            System.err.println("Массив пуст!");
+        }
+    }
+    
+    private void expand() {
+        int temporaryArray[] = new int[array.length * 2];
+        for (int i = 0; i < array.length; i++) {
+            temporaryArray[i] = array[i];
+        }
+        for (int i = array.length; i < temporaryArray.length; i++) {
+            temporaryArray[i] = 0;
+        }
+
+        array = temporaryArray;
+    }
+}
--- List/src/ru/karamoff/Main.java
+++ List/src/ru/karamoff/Main.java
@@ -1,12 +1,64 @@
 package ru.karamoff;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList list = new ArrayList();
-        list.addToEnd(7);
-        list.addToEnd(8);
-        list.addToEnd(9);
-        list.showList();
+        ExpandableArray array = new ExpandableArray();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Создать массив");
+            System.out.println("2. Добавить число в конец массива");
+            System.out.println("3. Удалить число из массива");
+            System.out.println("4. Вставить число в массив");
+            System.out.println("5. Вывести массив на экран");
+            System.out.println("6. Выход");
+            System.out.print("Выберите пункт меню: ");
+
+            int choice = scanner.nextInt();
+
+            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Введите количество элементов: ");
+                    int n = scanner.nextInt();
+                    System.out.println();
+                    System.out.println("Введите числа в количестве " + n + ":");
+                    while (array.count < n) {
+                        array.appendItem(scanner.nextInt());
+                    }
+                    System.out.println("Массив длиной " + n + " создан.");
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.print("Введите добавляемое число: ");
+                    array.appendItem(scanner.nextInt());
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.print("Введите номер удаляемого элемента (от 1 до " + array.count + "): ");
+                    array.removeItem(scanner.nextInt() - 1);
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.print("Введите номер вставляемого элемента (от 1 до " + array.count + "): ");
+                    int index = scanner.nextInt() - 1;
+                    System.out.print("Введите вставляемое число: ");
+                    int element = scanner.nextInt();
+                    array.insertItem(index, element);
+                    break;
+                case 5:
+                    array.printArray();
+                    break;
+                case 6:
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
