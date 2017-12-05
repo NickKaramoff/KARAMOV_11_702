4de54fcdf6b0799ec66c391e89f450edf266dbe7
Nick Karamoff <nikita.karamoff@gmail.com>
2017-12-05 12:37:27 +0300

    Add BinarySearch calsswork

---
+++ BinarySearch/src/ru/karamoff/BinarySearcher.java
@@ -0,0 +1,28 @@
+package ru.karamoff;
+
+public class BinarySearcher {
+    public static int search(int[] array, int start, int end, int toFind) {
+        System.out.println("Поиск в диапазоне от " + start + " до " + end + " включительно...");
+        int midPoint = start + (end - start) / 2;
+        if (end - start > 0) {
+            if (array[midPoint] > toFind) {
+                System.out.println("Элемент во первой половине, сужаем диапазон");
+                System.out.println();
+                return search(array, start, midPoint, toFind);
+            } else if (array[midPoint] < toFind) {
+                System.out.println("Элемент во второй половине, сужаем диапазон");
+                System.out.println();
+                return search(array, midPoint, end, toFind);
+            } else {
+                System.out.println("Элемент найден! Индекс: " + midPoint);
+                System.out.println();
+                return midPoint;
+            }
+        } else {
+            System.out.println("Элемент не найден!");
+            System.out.println();
+            return -1;
+        }
+
+    }
+}
---
+++ BinarySearch/src/ru/karamoff/Main.java
@@ -0,0 +1,19 @@
+package ru.karamoff;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] testArray = {63, 99, 31, 60, 54, 75, 3, 91, 38, 78, 44, 24, 18, 23, 5, 61, 40, 98, 30, 80};
+        int numberToFind = testArray[new Random().nextInt(testArray.length)];
+
+        Arrays.sort(testArray);
+        System.out.println("Отсортированный массив: " + Arrays.toString(testArray));
+        System.out.println("Искомое число: " + numberToFind);
+        System.out.println();
+
+        int a = BinarySearcher.search(testArray, 0, testArray.length - 1, numberToFind);
+    }
+}
